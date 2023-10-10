6c5da5617494687a0cec8423d548e131508f71b4
Wataru Masaka <watarubus00@gmail.com>
2023-10-10 14:28:45 +0900

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require_relative "../config/environment"
 require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
+ENV['RAILS_ENV'] ||= 'test'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -10,6 +11,7 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
