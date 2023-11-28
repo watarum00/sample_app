4bef67b2c6ebad0464eb6800be002afdf45a7819
watarum <watarubus00@gmail.com>
2023-11-28 21:34:56 +0900

    第8章の8.2まで

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      reset_session #ログインの直前に必ずこれをかく
+      log_in user
+      redirect_to user
+    else
+      #エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination' #本当は正しくない
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty? #フラッシュが空だとダメ
+    get root_path
+    assert flash.empty? #フラッシュが空でなければだめ
+  end
+
+  test "login with valid information" do
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0 #ログインパスのリンクがページにないかを判定
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+end
