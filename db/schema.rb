e6b13d9b23bd02465df7c40cffea3a98fbd3dd3f
watarum <watarubus00@gmail.com>
2023-11-29 17:12:05 +0900

    9.1.1記憶トークンと暗号化の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_073009) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_072123) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
