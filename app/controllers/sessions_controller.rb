e776025c7d25e183437ee7f05aa992ada5499189
watarum <watarubus00@gmail.com>
2023-11-29 17:34:00 +0900

    9.1.2 ログイン上の保持の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       reset_session #ログインの直前に必ずこれをかく
+      remember user
       log_in user
       redirect_to user
     else
