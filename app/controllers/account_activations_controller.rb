8172c568fec579ec2815e42a2989fa310d43b09e
watarum <watarubus00@gmail.com>
2023-12-17 22:57:40 +0900

    有効化フラグの変更の実装

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,17 @@
 class AccountActivationsController < ApplicationController
+
+    def edit
+        user = User.find_by(email: params[:email])
+        #一度有効化したら再度有効化しないようにする
+        if user && !user.activated? && user.authenticated?(:activation, params[:id])
+            user.update_attribute(:acitivated, true)
+            user.update_attribute(:activated_at, Time.zone.now)
+            log_in user
+            flash[:success] = "Account activated!"
+            redirect_to user
+        else
+            flash[:danger] = "Invalid activation link"
+            redirect_to root_url
+        end
+    end
 end
