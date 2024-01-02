468b630ae876d48f991f43851d49b26f209195af
watarum <watarubus00@gmail.com>
2024-01-02 15:19:10 +0900

    パスワード再設定のメールとテンプレート

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,7 +66,7 @@ class User < ApplicationRecord
 
     #パスワード再設定のメールを送信する
     def send_password_reset_email
-      UserMailer..password_reset(self).deliver_now
+      UserMailer.password_reset(self).deliver_now
     end
 
     private
