3b5f4fcdc945db21b768c77dfbfbb9fcd3268014
watarum <watarubus00@gmail.com>
2024-01-02 15:27:37 +0900

    送信メールのテスト追加

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -12,4 +12,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activation_token, mail.body.encoded
     assert_match CGI.escape(user.email), mail.body.encoded
   end
+
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)#送られるメール
+    assert_equal "Password reset", mail.subject#メールの件名が正しいかのテスト
+    assert_equal [user.email], mail.to#メールの送信先が正しいかテスト
+    assert_equal ["watarubus00@gmail.com"], mail.from#送信元が正しいかのテスト
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
+  end
 end
